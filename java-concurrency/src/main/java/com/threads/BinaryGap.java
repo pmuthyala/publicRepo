082d6bf7416c6b4507c18294d2f64904670f2245
Gopal Muthyala <Gopal.X.Muthyala.-ND@espn.com>
2018-01-22 13:04:22 -0500

    binary gap

--- java-concurrency/src/main/java/com/threads/BinaryGap.java
+++ java-concurrency/src/main/java/com/threads/BinaryGap.java
@@ -2,7 +2,7 @@ package com.threads;
 
 import java.util.stream.Stream;
 
-public class TestClass {
+public class BinaryGap {
 
 	public static void main(String []args) {
 		System.out.println(Integer.toBinaryString(12345));
---
+++ java-concurrency/src/main/java/com/threads/StringHashMap.java
@@ -0,0 +1,14 @@
+package com.threads;
+
+public class StringHashMap {
+
+	public static void main(String []args) {
+		String a = "Aa145699";
+		String b = "BB145799";
+		
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+		
+		System.out.println("1. Arizona Diamondbacks vs Atlanta Braves\n2. Chicago Cubs vs Baltimore Orioles\n3. Cleveland Indians vs Oakland Athletics\n4. Colorado Rockies vs New York Mets");
+	}
+}
