f72562d4948d756f48c772ac69f525b8064b1207
Gopal Muthyala <Gopal.X.Muthyala.-ND@espn.com>
2018-01-29 13:40:17 -0500

    simple test class

---
+++ java-test/src/main/java/com/test/TestJava.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class TestJava {
+
+	public TestJava() {
+		this(5);
+	}
+	
+	private int i;
+	public TestJava(int a) {
+		i = a;
+	}
+	
+	public static void main(String []a) {
+		TestJava t = new TestJava();
+		
+	}
+}
