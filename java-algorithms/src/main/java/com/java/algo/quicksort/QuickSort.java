b182339571cb90e3a0f423166ddbcd09fddf9771
Gopal Muthyala <pmuthyala@gmail.com>
2018-02-17 13:35:49 -0500

    added find kth element

--- java-algorithms/src/main/java/com/java/algo/quicksort/QuickSort.java
+++ java-algorithms/src/main/java/com/java/algo/quicksort/QuickSort.java
@@ -13,9 +13,27 @@ public class QuickSort {
 		
 		printArray("before start : ", arr);
 		sort(arr, 0, n - 1);
+		//int element = find5thElementByQuickSelect(arr, 0, n - 1, 5);
+		//System.out.println("7th element found is "+element);
 		printArray("after end    : ", arr);
 		
 	}
+	
+	static int find5thElementByQuickSelect(int []a, int low, int high, int k) {
+		if( k >= 0 && k <= high - low + 1) {
+			int pos = partition(a, low, high);
+			if(pos-low == k) {
+				return a[pos];
+			}
+			if(pos-low > k) {
+				return find5thElementByQuickSelect(a, low, pos-1, k);
+			}
+				
+			return find5thElementByQuickSelect(a, pos+1, high, k-pos+low-1);
+		}
+		return 0;
+	}
+	
 	static void sort(int []a, int low, int high) {
 		if(low < high) {
 			int pi = partition(a, low, high);
@@ -26,7 +44,7 @@ public class QuickSort {
 	
 	private static int partition(int []a, int low, int high) {
 		for(int j= low; j < high; j++) {
-			if(a[j] <= a[high]) {
+			if(a[j] < a[high]) {
 				int temp = a[low];
 				a[low] = a[j];
 				a[j] = temp;
